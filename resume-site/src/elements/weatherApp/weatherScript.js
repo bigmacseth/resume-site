4451bfefddfc019c9f5e2cc9335a909934ba1dd1
Seth Cradeur <bigmacseth@gmail.com>
2025-01-05 08:45:36 -0600

    Update weatherScript.js

--- resume-site/src/elements/weatherApp/weatherScript.js
+++ resume-site/src/elements/weatherApp/weatherScript.js
@@ -6,7 +6,7 @@ export function weatherScript() {
     var locationCall = 'https://maps.googleapis.com/maps/api/geocode/json?address=';
     var apiKey = '';
     
-    var weatherCall = 'http://api.weatherapi.com/v1/current.json?key=52e6756aa50d4c58bf7201050242710&q=';
+    var weatherCall = 'http://api.weatherapi.com/v1/current.json?key=*placeapikeyhere*=';
     
     
     enterEvent();
